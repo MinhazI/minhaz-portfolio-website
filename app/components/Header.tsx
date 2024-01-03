89e1641037726eb76dd6da4157fd4c8694841939
Minhaz Irphan Mohamed <MinhazI@users.noreply.github.com>
2024-01-03 10:12:10 +0530

    Updated the texts

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,12 +3,16 @@ import React from "react";
 function Header() {
   return (
     <div className="grid grid-cols-1 min-h-[100vh] text-center align-middle bg-gray-900">
-      <div className="flex flex-col justify-center text-white">
-        <p className="text-4xl">Minhaz I. Mohamed</p>
-        <p className="text-2xl pt-4">
+      <div className="flex flex-col justify-center text-white text-center">
+        <p className="text-4xl text-center">Hi there 👋!</p>
+        <p className="text-2xl pt-3">
+          My website is still under construction. Please visit this website
+          again later.
+        </p>
+        {/* <p className="text-2xl pt-4">
           My website is still under construction. Please visit this website
           later.
-        </p>
+        </p> */}
         <div className="pt-10">
           <p>
             Contact me:{" "}
